225b40643bc4fb17ebbccce5cd336500b26d3c60
jotarlo <Jeferson Arango Lpez>
2023-05-09 21:29:50 -0500

    Se agrega dotenv y lectura de variables de entorno

--- src/application.ts
+++ src/application.ts
@@ -1,11 +1,13 @@
+require('dotenv').config();
+
 import {BootMixin} from '@loopback/boot';
 import {ApplicationConfig} from '@loopback/core';
+import {RepositoryMixin} from '@loopback/repository';
+import {RestApplication} from '@loopback/rest';
 import {
   RestExplorerBindings,
   RestExplorerComponent,
 } from '@loopback/rest-explorer';
-import {RepositoryMixin} from '@loopback/repository';
-import {RestApplication} from '@loopback/rest';
 import {ServiceMixin} from '@loopback/service-proxy';
 import path from 'path';
 import {MySequence} from './sequence';
